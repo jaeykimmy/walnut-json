7d779ea447bbed987d4d98c5f0ff338e9bf3106a
Jaeyoung Kim <j.jaeykim@gmail.com>
2022-04-07 13:23:27 -0400

    put req not working

---
+++ app.js
@@ -0,0 +1,102 @@
+let express = require("express");
+let app = express();
+let json = require('./data.json');
+let bodyParser = require('body-parser');
+
+let jsonParser = bodyParser.json();
+
+// let urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
+
+
+
+app.get("/", (req, res, next) => {
+  res.json(json.recipes);
+});
+
+app.get("/recipes", (req, res, next) => {
+  let recipes = json.recipes;
+  let finalArr = [];
+  // for (let i = 0; i < recipes.length; i++) {
+  //   finalArr.push(recipes[i].name);
+  // }
+  
+  // recipes.forEach(obj => {
+  //   // console.log(obj);
+  //   finalArr.push(obj.name);
+  // });
+
+  let superArr = recipes.map(obj => {
+    return obj.name;
+  });
+
+  console.log(superArr);
+  res.json({ recipeName: superArr });
+});
+
+app.get(`/recipes/details/:recipeName`, (req, res, next) => {
+  let recipeName = req.params.recipeName;
+  let recipes = json.recipes;
+
+  
+  let ingredientArr = recipes.filter(obj => obj.name === recipeName);
+  if (ingredientArr.length === 0) {
+    res.json({});
+  } else {
+    res.json({
+      "details": {
+        "ingredients": ingredientArr[0].ingredients,
+        "numsteps": ingredientArr[0].ingredients.length
+      }});
+  }
+});
+
+app.post(`/recipes`, jsonParser, (req, res, next) => {
+  // console.log(req.body);
+  let recipes = json.recipes;
+  let duplicateRecipe = false;
+
+  for (let recipe of recipes) {
+    if (req.body.name === recipe.name) {
+      duplicateRecipe = true;
+      //check if theres a way to have line 66 stop line 71 from executing
+      break;
+    }
+  }
+  if (!duplicateRecipe) {
+    recipes.push(req.body);
+    res.sendStatus(200);
+  } else {
+    res.status(400).send({ "error": "Recipe already exists" });
+  }
+});
+
+app.put(`/recipes`, jsonParser, (req, res, next) => {
+  
+  let recipes = json.recipes;
+  let duplicateRecipe = false;
+
+  for (let recipe of recipes) {
+    if (req.body.name === recipe.name) {
+      duplicateRecipe = true;
+      break;
+    }
+  }
+  if (duplicateRecipe) {
+    console.log("reqname", req.body.name);
+    let index = recipes.indexOf(req.body.name);
+    if (index !== -1) {
+      recipes[index] = req.body;
+    }
+    res.sendStatus(200);
+  } else {
+    res.status(400).send({ "error": "Recipe doesn't exist" });
+  }
+
+  console.log(recipes.indexOf(req.body));
+
+  
+});
